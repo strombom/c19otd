2e7f501f677f5f88dac6e44ca88a6d57f1ba8db6
Johan Strombom <strombom@gmail.com>
2020-07-11 16:34:25 +0200

    Constants

--- backtrace.py
+++ backtrace.py
@@ -17,18 +17,18 @@ otd_samples = [20, 15, 6, 10, 14, 41, 12, 30, 7, 19, 13, 10, 11, 19, 16, 17, 24,
 otd_shape, otd_loc, otd_scale = lognorm.fit(otd_samples)
 eto_samples = [5, 4, 6, 5, 29, 4, 6, 8, 7, 21, 1, 7, 8, 8, 9, 3, 8, 4, 5, 3, 4, 11, 5, 9, 4, 3, 6, 5, 6, 11, 12, 4, 8, 14, 3, 7, 28, 5, 6, 9, 3, 2, 2, 14, 32, 7, 2, 15, 8, 9, 3, 2, 2, 14, 32, 7, 2, 15, 8]
 eto_shape, eto_loc, eto_scale = lognorm.fit(eto_samples)
+truncate_tail = 50
 
 def sample_exposure_to_death():
     exposure_to_onset = lognorm.rvs(s=eto_shape, loc=eto_loc, scale=eto_scale)
     onset_to_death = math.inf
-    truncate = 50
-    while onset_to_death > truncate:
+    while onset_to_death > truncate_tail:
         onset_to_death = lognorm.rvs(s=otd_shape, loc=otd_loc, scale=otd_scale)
     return int(exposure_to_onset + onset_to_death)
 
 
 # Data souce (FHM) https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/bekraftade-fall-i-sverige/
-new_deaths = np.concatenate((np.zeros(71, dtype=np.int), np.array([1, 0, 1, 1, 2, 2, 1, 6, 7, 9, 8, 11, 11, 21, 22, 31, 32, 35, 38, 45, 48, 53, 70, 80, 70, 85, 90, 84, 115, 86, 90, 103, 97, 85, 91, 115, 111, 82, 86, 88, 84, 62, 77, 86, 89, 73, 75, 73, 82, 84, 78, 78, 73, 75, 84, 72, 73, 80, 60, 67, 74, 64, 61, 50, 46, 57, 48, 53, 61, 39, 54, 53, 55, 56, 43, 42, 28, 39, 40, 40, 39, 45, 40, 36, 26, 45, 38, 29, 33, 38, 34, 40, 34, 29, 33, 26, 29, 28, 32, 29, 28, 28, 20, 19, 25, 22, 20, 9, 13, 20, 17, 15, 9, 13, 6, 8, 5, 7, 4, 1, 2, 0]), np.zeros(50, dtype=np.int)))
+new_deaths = np.concatenate((np.zeros(lockdown_day_of_year - 1, dtype=np.int), np.array([1, 0, 1, 1, 2, 2, 1, 6, 7, 9, 8, 11, 11, 21, 22, 31, 32, 35, 38, 45, 48, 53, 70, 80, 70, 85, 90, 84, 115, 86, 90, 103, 97, 85, 91, 115, 111, 82, 86, 88, 84, 62, 77, 86, 89, 73, 75, 73, 82, 84, 78, 78, 73, 75, 84, 72, 73, 80, 60, 67, 74, 64, 61, 50, 46, 57, 48, 53, 61, 39, 54, 53, 55, 56, 43, 42, 28, 39, 40, 40, 39, 45, 40, 36, 26, 45, 38, 29, 33, 38, 34, 40, 34, 29, 33, 26, 29, 28, 32, 29, 28, 28, 20, 19, 25, 22, 20, 9, 13, 20, 17, 15, 9, 13, 6, 8, 5, 7, 4, 1, 2, 0]), np.zeros(truncate_tail, dtype=np.int)))
 n_samples = len(new_deaths)
 t = np.linspace(0, n_samples, n_samples)
 
